7303e8dc0f290d54967ae4c80c585213d57ac143
Evandro Schuck <dev@evandro.us>
2022-10-09 20:39:35 -0300

    fix change mobile icon

--- body.before.js
+++ body.before.js
@@ -5,13 +5,13 @@ $(document).ready(function () {
         $('.e-100>div:first-child>div:first-child').css('height', '250%');
         $('.virtual-100>div:first-child>div:first-child').css('height', '250%');
         $('#column2463').removeAttr("style");
+        $('.eruTRg:eq(2) span svg').remove();
+        $('.eruTRg:eq(2) span').prepend('<img src="https://bet-files.suprema.group/betslip.png" width="24px" height="24px">');
     }, 3000);
     $('.promotion-filters').css('border-bottom', '1px solid #000!important');
     $('.header-rows .ModuleNavigation>.navigation-inner:not(.in-canvas)>.off-canvas-button').remove();
     $('#column2463').removeAttr("style");
     $('div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
-    $('.eruTRg:eq(2) span svg').remove();
-    $('.eruTRg:eq(2) span').prepend('<img src="https://bet-files.suprema.group/betslip.png" width="24px" height="24px">');
 });
 document.querySelector(".footerbuttonup").onclick = function () {
     expand()

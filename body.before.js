310013bac8d7cd4e1a103268ae3da1442886240d
Murilo Henrique Prudencio <murilo.prudencio@outlook.com>
2022-10-10 11:23:50 -0300

    ajustes

--- body.before.js
+++ body.before.js
@@ -5,12 +5,26 @@ $(document).ready(function () {
         $('.e-100>div:first-child>div:first-child').css('height', '250%');
         $('.virtual-100>div:first-child>div:first-child').css('height', '250%');
         $('#column2463').removeAttr("style");
+        $('.eruTRg:eq(2) span svg').remove();
+        $('.eruTRg:eq(2) span').prepend('<img src="https://bet-files.suprema.group/betslip.png" width="24px" height="24px">');
+        $('.mobile .eruTRg').on('click', function () {
+            $('.close-widgets-container').addClass('hidden');
+            setTimeout(function () {
+                $('.mobile .cetaLz').on('click', function () {
+                    $('.close-widgets-container').removeClass('hidden');
+                });
+            }, 1000);
+        });
     }, 3000);
     $('.promotion-filters').css('border-bottom', '1px solid #000!important');
     $('.header-rows .ModuleNavigation>.navigation-inner:not(.in-canvas)>.off-canvas-button').remove();
     $('#column2463').removeAttr("style");
-    $('div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.sportsPC div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=sports&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.sportsMobile div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=msports&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.cassinoPC div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=casino&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.cassinoMobile div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=mcasino&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
 });
+
 document.querySelector(".footerbuttonup").onclick = function () {
     expand()
 };

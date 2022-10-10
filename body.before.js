a461d67d5035b43714ebb0a95f3b0669a8e444dd
Evandro Schuck <dev@evandro.us>
2022-10-10 16:23:02 -0300

    fix scripts

--- body.before.js
+++ body.before.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $('.prema-100>div:first-child>div:first-child').css('top', '15em');
     setTimeout(function () {
@@ -23,37 +24,28 @@ $(document).ready(function () {
     $('.promotion-filters').css('border-bottom', '1px solid #000!important');
     $('.header-rows .ModuleNavigation>.navigation-inner:not(.in-canvas)>.off-canvas-button').remove();
     $('#column2463').removeAttr("style");
-    $('.sportsPC div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=sports&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
-    $('.sportsMobile div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=msports&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
-    $('.cassinoPC div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=casino&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
-    $('.cassinoMobile div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=mcasino&l=${window.language}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    const lang = JSON.parse(window.hrefLangRelations);
+    $('.sportsPC div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=sports&l=${lang.currentPrefix}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.sportsMobile div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=msports&l=${lang.currentPrefix}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.cassinoPC div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=casino&l=${lang.currentPrefix}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.cassinoMobile div.slider-container').html(`<iframe src="https://pokerbyte.com.br/bet/banner?t=mcasino&l=${lang.currentPrefix}" width="100%" height="100%" scrolling="no" style="border:none;"></iframe>`);
+    $('.footerbuttonup').on('click', function () {
+        if (document.querySelector(".footer").style.height = "10px") {
+            document.querySelector(".footer").style.height = "235px";
+            document.querySelector(".footerbuttondown").style.visibility = "visible";
+            document.querySelector(".footerbuttonup").style.visibility = "hidden";
+            document.querySelector(".footercont").style.opacity = "1";
+            document.querySelector(".footercont").style.visibility = "visible";
+        }
+    });
+    $('.footerbuttondown').on('click', function () {
+        if (document.querySelector(".footer").style.height = "235px") {
+            document.querySelector(".footer").style.height = "10px";
+            document.querySelector(".footerbuttondown").style.visibility = "hidden";
+            document.querySelector(".footerbuttonup").style.visibility = "visible";
+            document.querySelector(".footercont").style.opacity = "0";
+            document.querySelector(".footercont").style.visibility = "hidden";
+        }
+    });
+    document.querySelector(".footer").style.height = "10px";
 });
-
-document.querySelector(".footerbuttonup").onclick = function () {
-    expand()
-};
-document.querySelector(".footerbuttondown").onclick = function () {
-    shrink()
-};
-document.querySelector(".footer").style.height = "10px";
-
-function expand() {
-    if (document.querySelector(".footer").style.height = "10px") {
-        document.querySelector(".footer").style.height = "235px";
-        document.querySelector(".footerbuttondown").style.visibility = "visible";
-        document.querySelector(".footerbuttonup").style.visibility = "hidden";
-        document.querySelector(".footercont").style.opacity = "1";
-        document.querySelector(".footercont").style.visibility = "visible";
-    }
-}
-
-function shrink() {
-    if (document.querySelector(".footer").style.height = "235px") {
-        document.querySelector(".footer").style.height = "10px";
-        document.querySelector(".footerbuttondown").style.visibility = "hidden";
-        document.querySelector(".footerbuttonup").style.visibility = "visible";
-        document.querySelector(".footercont").style.opacity = "0";
-        document.querySelector(".footercont").style.visibility = "hidden";
-    }
-}
-

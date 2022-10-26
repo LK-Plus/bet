1a5704a581ba58842b71cf8d87f24ffd6f600686
CarlosPirico <carlos.pirico@hotmail.com>
2022-10-26 10:21:04 -0300

    remoção indicar amigo

--- body.before.js
+++ body.before.js
@@ -153,14 +153,21 @@ document.addEventListener('DOMContentLoaded', function() {
     // Fix CPF check
     setInterval(carregaCpf, 2000);
 
-    // Mudança de texto verificação de documento
+    // Modal info player
     if (document.querySelector('.loginDesk,.loginMobile')) {
         document.querySelector('body').addEventListener("DOMSubtreeModified", function() {
             if(document.querySelector('.accountModal')) {
+                //alteração texto upload arquivos
                 if(document.querySelector('.uploadInfo__sectionItem')) {
                     if (document.querySelector('.uploadInfo__sectionItem').innerHTML == '<div>• A foto deve ser tirada do documento original, não é permitido fazer qualquer montagem de foto digital.</div><div>• A selfie deve ser tirada em local claro e estar nítida.</div><div>• A imagem do documento, dados pessoais, assinatura, selo e outras informações devem ser claramente legíveis. • Se necessário, a empresa poderá solicitar a apresentação de outros documentos. ***Obs: Caso haja qualquer dúvida no processo, entre em contato com nosso suporte, clicando no menu SUPORTE, na parte superior direita do site.</div> ') {
                         document.querySelector('.uploadInfo__sectionItem').innerHTML = '<div>• A foto deve ser tirada do documento original, não é permitido fazer qualquer montagem de foto digital.</div><div>• A selfie deve ser tirada em local claro e estar nítida.</div><div>• A imagem do documento, dados pessoais, assinatura, selo e outras informações devem ser claramente legíveis.</div><div>• Se necessário, a empresa poderá solicitar a apresentação de outros documentos.</div><div><b>Obs: Caso haja qualquer dúvida no processo, entre em contato com nosso suporte, clicando no menu SUPORTE, na parte superior direita do site.</b></div>'
                     };
+                };
+                // remoção de convidar um amigo
+                if(document.querySelector('accMenu')){
+                    if($('.accMenu .accMenu__item:nth-child(2)').text() == 'Indicar um amigo'){
+                        $('.accMenu .accMenu__item:nth-child(2)').remove()
+                    }
                 };
             };
         });

c61f77e202b0341dc1b4561882cbd7557b8628b2
CarlosPirico <carlos.pirico@hotmail.com>
2022-10-31 17:20:09 -0300

    messsagem

--- body.before.js
+++ body.before.js
@@ -218,7 +218,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     }
                 } else {
                     document.querySelector('.notificationMessage').remove()
-                    document.querySelector('.click-close').click()
+                    document.getElementById('btn-webchat').click()
                 }
             };
         });

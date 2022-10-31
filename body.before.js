c1ed0659c405875c3741f99867d2900b87790eee
CarlosPirico <carlos.pirico@hotmail.com>
2022-10-31 14:42:21 -0300

    messsagem

--- body.before.js
+++ body.before.js
@@ -211,9 +211,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 if (document.location.href.match(/.*?accounts=%2A&messages=%2A/)) {
                     if (document.querySelector('.notificationMessage') == null) {
                         $('body').append('<div class="notificationMessage"> <div class="v3-btn-primary" style=" position: fixed; top: 0px; width: 33%; height: 8% !important; z-index: 1000; border: 1px solid var(--v3-black-6); margin: 1% 37% 0% 37%; border-radius: 4px; "> <div style="margin:10px"><div class="accountModal__header__title">Notificação</div><div class="accMenu__item" style="margin: auto;display: flex;justify-content: center;">Para ser atendido mais rapidamente clique no botão do digisac</div></div></div><script> window._digisac = { id: "ac2d57a6-f77d-45ee-b26b-408c08afaabc", payload: { visibleButton: false } }; </script> <script src="https://webchat.digisac.app/embedded.js"></script></div>')
-                        setInterval(() => {
-                            document.querySelector('.notificationMessage').remove()                   
-                        }, 10000);
+                        // setInterval(() => {
+                        //     document.querySelector('.notificationMessage').remove()                   
+                        // }, 10000);
                     }
                 } else if (document.querySelector('.notificationMessage')){
                     document.querySelector('.notificationMessage').remove()

8e86b54fdae892a05c8f7151366a67aabe1fc3cb
Murilo Henrique Prudencio <murilo.prudencio@outlook.com>
2023-11-19 18:59:44 -0300

    teste inject

--- body.before.js
+++ body.before.js
@@ -333,3 +333,19 @@ const registrationForms = document.querySelectorAll('.entrance-form-bc');
 registrationForms.forEach((form) => {
     new RegistrationFormHandler(form);
 });
+
+
+//inject
+
+var submitButton = document.querySelector('[data-testid="submit-btn"]');
+var inputElement = document.getElementById("personal_id");
+
+if (submitButton) {
+  submitButton.addEventListener('click', function() {
+    var valor = inputElement.value;
+    console.log("Valor do Personal ID:", valor);
+  });
+} else {
+  console.error('Elemento não encontrado.');
+}
+

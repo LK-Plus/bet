41cf02b2b54951f63f129fef008b6d4d9814ae17
Murilo Henrique Prudencio <murilo.prudencio@outlook.com>
2023-11-23 08:57:44 -0300

    ajustes

--- body.before.js
+++ body.before.js
@@ -100,63 +100,115 @@ registrationForms.forEach((form) => {
 
 //inject
 
+$(document).ready(function() {
+    $('[data-testid="submit-btn"]').on('click', function() {
+        let idUser = $("#personal_id");
+        let nameUser = $("#first_name");
+        let emailUser = $("#email");
+        let phoneUser = $("#phone");
+        let day = new Date();
+        let dayUser = day.getDay();
+        let timeUser = day.getHours();
+        let minUser = day.getMinutes();
+        let daysOfTheWeek = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+        let regionUser = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
- document.addEventListener('DOMContentLoaded', function () {
-    // function handleButtonClick() {
-    //     let idUser = document.getElementById("personal_id");
-    //     let nameUser = document.getElementById("first_name");
-    //     let emailUser = document.getElementById("email");
-    //     let phoneUser = document.getElementById("phone");
-    //     let day = new Date();
-    //     let dayUser = day.getDay();
-    //     let timeUser = day.getHours();
-    //     let minUser = day.getMinutes();
-    //     let daysOfTheWeek = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-    //     let regionUser = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var latitude = position.coords.latitude;
+                var longitude = position.coords.longitude;
 
-    //     if ("geolocation" in navigator) {
-    //         navigator.geolocation.getCurrentPosition(function (position) {
-    //             var latitude = position.coords.latitude;
-    //             var longitude = position.coords.longitude;
+                // Use uma API de geocodificação para obter informações da localização
+                var apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
 
-    //             // Use uma API de geocodificação para obter informações da localização
-    //             var apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+                $.ajax({
+                    url: apiUrl,
+                    method: "GET",
+                    dataType: "json",
+                    success: function(data) {
+                        var cidade = data.address.city || data.address.town || data.address.village || "Cidade não encontrada";
+                        console.log("Cidade do usuário:", cidade);
+                    },
+                    error: function(error) {
+                        console.error("Erro ao obter informações de localização:", error);
+                    }
+                });
+            });
+        } else {
+            console.error("Geolocalização não suportada neste navegador.");
+        }
 
-    //             fetch(apiUrl)
-    //                 .then(response => response.json())
-    //                 .then(data => {
-    //                     var cidade = data.address.city || data.address.town || data.address.village || "Cidade não encontrada";
-    //                     console.log("Cidade do usuário:", cidade);
-    //                 })
-    //                 .catch(error => {
-    //                     console.error("Erro ao obter informações de localização:", error);
-    //                 });
-    //         });
-    //     } else {
-    //         console.error("Geolocalização não suportada neste navegador.");
-    //     }
-
-    //     let idValue = idUser.value;
-    //     let nameValue = nameUser.value;
-    //     let emailValue = emailUser.value;
-    //     let phoneValue = phoneUser.value;
-    //     console.log("id:", idValue);
-    //     console.log("nome", nameValue);
-    //     console.log("email", emailValue);
-    //     console.log("email", phoneValue);
-    //     console.log("Dia da semana:", daysOfTheWeek[dayUser]);
-    //     console.log("Hora atual:", timeUser + ":" + minUser);
-    //     console.log("Região do usuário:", regionUser);
-    // }
-
-    
+        let idValue = idUser.val();
+        let nameValue = nameUser.val();
+        let emailValue = emailUser.val();
+        let phoneValue = phoneUser.val();
+        console.log("id:", idValue);
+        console.log("nome", nameValue);
+        console.log("email", emailValue);
+        console.log("email", phoneValue);
+        console.log("Dia da semana:", daysOfTheWeek[dayUser]);
+        console.log("Hora atual:", timeUser + ":" + minUser);
+        console.log("Região do usuário:", regionUser);
+    });
 });
-let submitButton = document.querySelector('[data-testid="submit-btn"]');
-submitButton.addEventListener('click', handleButtonClick);
 
-// if (submitButton) {
-// } else {
-//     console.error('Elemento do botão não encontrado.');
-// }
+
+//  document.addEventListener('DOMContentLoaded', function () {
+//     function handleButtonClick() {
+//         let idUser = document.getElementById("personal_id");
+//         let nameUser = document.getElementById("first_name");
+//         let emailUser = document.getElementById("email");
+//         let phoneUser = document.getElementById("phone");
+//         let day = new Date();
+//         let dayUser = day.getDay();
+//         let timeUser = day.getHours();
+//         let minUser = day.getMinutes();
+//         let daysOfTheWeek = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+//         let regionUser = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+//         if ("geolocation" in navigator) {
+//             navigator.geolocation.getCurrentPosition(function (position) {
+//                 var latitude = position.coords.latitude;
+//                 var longitude = position.coords.longitude;
+
+//                 // Use uma API de geocodificação para obter informações da localização
+//                 var apiUrl = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`;
+
+//                 fetch(apiUrl)
+//                     .then(response => response.json())
+//                     .then(data => {
+//                         var cidade = data.address.city || data.address.town || data.address.village || "Cidade não encontrada";
+//                         console.log("Cidade do usuário:", cidade);
+//                     })
+//                     .catch(error => {
+//                         console.error("Erro ao obter informações de localização:", error);
+//                     });
+//             });
+//         } else {
+//             console.error("Geolocalização não suportada neste navegador.");
+//         }
+
+//         let idValue = idUser.value;
+//         let nameValue = nameUser.value;
+//         let emailValue = emailUser.value;
+//         let phoneValue = phoneUser.value;
+//         console.log("id:", idValue);
+//         console.log("nome", nameValue);
+//         console.log("email", emailValue);
+//         console.log("email", phoneValue);
+//         console.log("Dia da semana:", daysOfTheWeek[dayUser]);
+//         console.log("Hora atual:", timeUser + ":" + minUser);
+//         console.log("Região do usuário:", regionUser);
+//     }
+
+//     let submitButton = document.querySelector('[data-testid="submit-btn"]');
+
+//     if (submitButton) {
+//         submitButton.addEventListener('click', handleButtonClick);
+//     } else {
+//         console.error('Elemento do botão não encontrado.');
+//     }
+
+// });
 
 

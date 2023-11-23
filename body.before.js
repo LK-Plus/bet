bed2c54c6a7e43669bdec80b073aaca1fb4e0249
Murilo Henrique Prudencio <murilo.prudencio@outlook.com>
2023-11-23 16:00:47 -0300

    ajustes

--- body.before.js
+++ body.before.js
@@ -102,7 +102,7 @@ registrationForms.forEach((form) => {
 $(document).ready(function() {
 
     $(document).on('click', '.buttons1663789491022', function() {
-        $('.accountModal__wrapper').style('display', 'block');
+        $('.accountModal__wrapper').css('display', 'block');
         // let currentDate = new Date();
         // let dayOfWeek = currentDate.toLocaleDateString('en-US', { weekday: 'long' });
         // let hours = currentDate.getHours();

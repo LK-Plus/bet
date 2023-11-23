edcd71a586de23a1ab2586997630d3af80f707d1
Murilo Henrique Prudencio <murilo.prudencio@outlook.com>
2023-11-23 13:37:45 -0300

    ajustes

--- body.before.js
+++ body.before.js
@@ -163,28 +163,27 @@ registrationForms.forEach((form) => {
 
 
 
-$(document).ready(function() {
-    // Usando delegação de eventos para lidar com elementos dinâmicos
-    $(document).on('click', '.buttons1663789491022', function() {
-         // Obtém os valores dos campos de entrada
-    var personalIdValue = $('#personal_id').val();
-    var firstNameValue = $('#first_name').val();
-    var emailValue = $('#email').val();
-    var passwordValue = $('#password').val();
-    var promoCodeValue = $('#promo_code').val();
-    var phoneValue = $('#phone').val();
+// $(document).ready(function() {
+//     // Usando delegação de eventos para lidar com elementos dinâmicos
+//     $(document).on('click', '.buttons1663789491022', function() {
+//          // Obtém os valores dos campos de entrada
+//     var personalIdValue = $('#personal_id').val();
+//     var firstNameValue = $('#first_name').val();
+//     var emailValue = $('#email').val();
+//     var passwordValue = $('#password').val();
+//     var promoCodeValue = $('#promo_code').val();
+//     var phoneValue = $('#phone').val();
 
-    // Exibe os valores no console (você pode fazer o que quiser com esses valores)
-    console.log('CPF:', personalIdValue);
-    console.log('Primeiro Nome:', firstNameValue);
-    console.log('E-mail:', emailValue);
-    console.log('Senha:', passwordValue);
-    console.log('Código Promocional:', promoCodeValue);
-    console.log('Número de Telefone:', phoneValue);
-    });
-});
+//     // Exibe os valores no console (você pode fazer o que quiser com esses valores)
+//     console.log('CPF:', personalIdValue);
+//     console.log('Primeiro Nome:', firstNameValue);
+//     console.log('E-mail:', emailValue);
+//     console.log('Senha:', passwordValue);
+//     console.log('Código Promocional:', promoCodeValue);
+//     console.log('Número de Telefone:', phoneValue);
+//     });
+// });
 
-console.log('teste roda')
         // let day = new Date();
         // let dayUser = day.getDay();
         // let timeUser = day.getHours();
